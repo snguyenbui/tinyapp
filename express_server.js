6263807c5b9f3c0c395f0222f1cc4c76284874d2
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-16 20:00:22 +0000

    wrapped project in nodemon

--- express_server.js
+++ express_server.js
@@ -42,12 +42,18 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect("/urls/" + newShortURL);
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
