8d69363e43a7ed1e080cb6ab0d289518567ba2af
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-17 01:09:27 +0000

    Added logout to _header and username display

--- express_server.js
+++ express_server.js
@@ -43,6 +43,11 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 })
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+
 app.get("/urls", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
